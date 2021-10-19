4fee370cd1eb85e27491abc6017aacd761c94dc1
ShaneMPutnam <sputnam@dewberry.com>
2021-10-19 13:53:46 -0400

    Rename reduced to just excess precipitation

--- extract_excess_precip.py
+++ extract_excess_precip.py
@@ -18,16 +18,19 @@ def extract_event_totals(forcing_files: list) -> dict:
     return event_totals
 
 
-def create_event_total_dataframe(reduced_event_totals: dict, unreduced_event_totals: dict, domain: str) -> pd.DataFrame:
-    """Create a dataframe of the reduced and unreduced excess precipitation for the specified domain."""
-    event_totals_df = pd.DataFrame.from_dict({"Reduced_Excess_P": reduced_event_totals[domain]})
+def create_event_total_dataframe(event_totals: dict, unreduced_event_totals: dict, domain: str) -> pd.DataFrame:
+    """Create a dataframe of the excess precipitation and unreduced excess precipitation (if applicable) for the
+    specified domain."""
+    event_totals_df = pd.DataFrame.from_dict({"Excess_P": event_totals[domain]})
     if domain in unreduced_event_totals.keys():
         unreduced_event_totals_df = pd.DataFrame.from_dict({"Unreduced_Excess_P": unreduced_event_totals[domain]})
         if not all(unreduced_event_totals_df.index == event_totals_df.index):
-            raise ValueError("The reduced and unreduced event IDs do not match, cannot concatenate the dataframes")
+            raise ValueError(
+                "The excess precipitation and unreduced event IDs do not match, cannot concatenate the dataframes"
+            )
         event_totals_df = pd.concat([unreduced_event_totals_df, event_totals_df], axis=1)
-        if not all(event_totals_df["Reduced_Excess_P"] <= event_totals_df["Unreduced_Excess_P"]):
-            raise ValueError("One or more events have reduced excess precipitation that is greater than the unreduced.")
+        if not all(event_totals_df["Excess_P"] <= event_totals_df["Unreduced_Excess_P"]):
+            raise ValueError("One or more events have excess precipitation that is greater than the unreduced.")
     event_totals_df.sort_index(inplace=True)
     event_totals_df.index.name = "Event_ID"
     return event_totals_df
@@ -43,11 +46,11 @@ forcing_files = list(forcing_dir.glob("**/*.json"))
 unreduced_forcing_files = [file for file in metadata_dir.glob("**/*.json") if "Unreduced" in file.stem]
 print(f"{len(forcing_files)} forcing files and {len(unreduced_forcing_files)} unreduced forcing files identified")
 
-reduced_event_totals = extract_event_totals(forcing_files)
+event_totals = extract_event_totals(forcing_files)
 unreduced_event_totals = extract_event_totals(unreduced_forcing_files)
 
 writer = pd.ExcelWriter(event_excess_path)
-for domain in reduced_event_totals.keys():
-    event_totals_df = create_event_total_dataframe(reduced_event_totals, unreduced_event_totals, domain)
+for domain in event_totals.keys():
+    event_totals_df = create_event_total_dataframe(event_totals, unreduced_event_totals, domain)
     event_totals_df.to_excel(writer, sheet_name=f"{sheet_name_prefix}_{domain}")
 writer.save()
